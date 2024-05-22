83fa437053251a3c9849be66a0c67bf6003e4bc8
savan <savanparvadiya31@gmail.com>
2024-05-22 00:45:30 -0400

    sending Response

--- createServer.js
+++ createServer.js
@@ -1,7 +1,10 @@
 const http = require('http'); //will call http module
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url,req.headers, req.method); //When client send req on our server, It will give some data which client can't see so you can provide them like this
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><head><title>Welcome to the website</title></head><body>hey Welcome</body></html>') //Showing manual data on website
+    res.end();
 }) //to create server
 
 server.listen(7000); //basically your server will listen on port 7000
